aaa42ac5d3ccf129d70dda6ccd3aadb82c697780
ClaireBadouel <clairebadouel@outlook.fr>
2023-06-05 18:24:17 +0200

    clean

--- scripts/config.js
+++
@@ -1,7 +0,0 @@
-const config_URL = {
-talent_status : "http://localhost:3000/palm/api/v1/talent_status/",
-talent_units :  "http://localhost:3000/palm/api/v1/talent_units/",
-talent_grade : "http://localhost:3000/palm/api/v1/talent_grades/"
-};
-
-export default config_URL
